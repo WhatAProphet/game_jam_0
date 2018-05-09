270ffd7bbb2d0970145f181d5ea7561e16528b0f
Unknown <eti-ya@live.ca>
2018-05-09 16:25:56 -0400

    f

---
+++ ball.py
@@ -0,0 +1,18 @@
+import pygame
+import time
+from pygame.locals import *
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("Game Jam 0")
+    screen = pygame.display.set_mode((500,500))
+    time.sleep(5)
+    
+
+
+
+class Ball:
+    
+    
+if __name__ == "__main__":
+    main()
